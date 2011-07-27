cf8104cc53efeb29522216b2a95889787c701012
Alex Paul <alex@alexpaul.net>
2011-07-26 17:14:12 -0700

    Created stub

---
+++ app.js
@@ -0,0 +1,9 @@
+
+modules = []
+
+//TODO: include couch.js
+//TODO: include all modules that we want to use
+
+
+//Start the app
+db = new CouchDB("hudwhere", null);
---
+++ machine.js
@@ -0,0 +1,22 @@
+
+modules.push(new MachineModule());
+
+function MachineModule() {
+    this.install = function() {
+        //TODO
+    }
+
+    this.uninstall = function() {
+        //TODO
+    }
+
+    this.get_views(entity) {
+        //TODO
+        return [];
+    }
+
+    this.validate(entity) {
+        //TODO
+        return true;
+    }
+}
