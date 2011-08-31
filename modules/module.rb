13eb14400e7033fe326af230688e59cb0b09fcaa
Alex Paul <alex@alexpaul.net>
2011-08-31 13:32:03 -0700

    switching machines, incomplete

---
+++ modules/module.rb
@@ -0,0 +1,24 @@
+
+# Some stubs
+class Module
+  def install()
+
+  end
+
+  def uninstall()
+
+  end
+
+  # Validate the given mongo entity.
+  # Return true if valid or unknown
+  # Return false if invalid
+  def validate(entity)
+    return true
+  end
+
+  # 
+  def get_views(entity)
+    return []
+
+  end
+end
